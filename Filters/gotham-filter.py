74ff238b0304ebe1d87b5d119707ebf1dd4d21fb
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-01-23 12:38:00 +0530

    base code

---
+++ Filters/gotham-filter.py
@@ -0,0 +1,32 @@
+#gotham filter code
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage
+from skimage import data
+%matplotlib inline
+
+from skimage import data, filters, io
+from skimage.io import imread
+
+def get_image(url):
+	image = imread(url) 
+	original_image = skimage.img_as_float(image)
+	return original_image
+
+
+show = lambda x: io.imshow(x)
+
+def split_image_into_channels(image):
+	red_channel   = image[:,:,0]
+	green_channel = image[:,:,1]
+	blue_channel  = image[:,:,2]
+
+
+
+image= get_image("http://www.toursara.com/images/geo_pic.1363.10.jpg")
+show(image)
+
+r,g,b = split_image_into_channelsimage()
+
